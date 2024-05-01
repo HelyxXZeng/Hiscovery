23fd935fa5dd0d66b95f3b4bafe1b38506428c39
Bow Jaro <100690848+BowJaro@users.noreply.github.com>
2024-05-01 15:07:19 +0700

    create UserStatusComponent

--- Hiscovery/app/(tabs)/following/index.js
+++ Hiscovery/app/(tabs)/following/index.js
@@ -3,16 +3,29 @@ import { StyleSheet, Text, View } from "react-native";
 import Header from "../../../components/header/Header";
 import OnThisDay from '../../../components/event/OnThisDay'
 import AuthorProfile from '../../../components/profile/AuthorProfile'
+import UserStatusComponent from "../../../components/user-status/UserStatusComponent";
 
 export default function Page() {
+  const userData = {
+    id: 1,
+    username: "john_doe",
+    name: "John Doe",
+    email: "john@example.com",
+    phone: "123-456-7890",
+    join_date: new Date("2023-01-15"),
+    birthdate: new Date("1990-05-01"),
+    image_url: "https://th.bing.com/th/id/OIP.Xm76x6WrXpgml5uZIo5o0gHaGL?w=201&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+    status: "active"
+  };
+
+
   return (
     <View style={styles.container}>
       <Stack.Screen
         options={{
           headerTitle: () => <Header title="Following" iconvisible={false} />,
         }} />
-      {/* <OnThisDay /> */}
-      {/* <AuthorProfile id={38} /> */}
+      <UserStatusComponent data={userData} />
       <Text>Index page of Following Tab</Text>
     </View>
   );
