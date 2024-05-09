8fa742cc616beb9462170a40fb933e0b0116f332
Bow Jaro <100690848+BowJaro@users.noreply.github.com>
2024-05-09 10:54:11 +0700

    finish Search page (not search hash tag)

--- Hiscovery/app/search/[keyword].tsx
+++ Hiscovery/app/search/[keyword].tsx
@@ -1,55 +1,108 @@
+import React from 'react';
 import { useRoute } from '@react-navigation/native';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import BigArticleList from '../../components/article-list/BigArticleList'
-import { TextInput, TouchableOpacity, View, StyleSheet } from 'react-native';
+import { TextInput, TouchableOpacity, View, StyleSheet, ScrollView, Text } from 'react-native';
 import { icons, COLORS } from '../../constants';
-import { useRouter } from 'expo-router';
+import { Stack, useRouter } from 'expo-router';
+import Header from "../../components/header/Header";
+import { supabase } from "../../lib/supabase";
 
 const SearchPage = () => {
   const router = useRouter()
   const route = useRoute();
   const { keyword } = route.params //This has compile error but can run without problem
+  const decodedKeyword = decodeURIComponent(keyword);
   const [searchValue, setSearchValue] = useState('');
   const [articles, setArticles] = useState([]);
+  const [readerId, setReaderId] = useState(0)
+  const [isLoading, setIsLoading] = useState(true);
 
-  const fetchArticlesByKeyword = async (keyword) => {
-    // This is an empty function. Replace this with your actual data fetching logic.
-    const data = [];
-    setArticles(data);
+  const getId = async () => {
+    const { data: { user } } = await supabase.auth.getUser()
+    let { data, error } = await supabase
+      .rpc('get_id_by_email', {
+        p_email: user.email
+      })
+    if (error) console.error(error)
+    else {
+      setReaderId(data);
+      // console.log('Id here', data)
+    }
+  }
+
+  const fetchArticlesByKeyword = async () => {
+    const search = async () => {
+      let { data, error } = await supabase
+        .rpc('search_articles', {
+          keyword: decodedKeyword,
+          user_id: readerId
+        })
+      if (error) console.error(error)
+      else {
+        setArticles(data);
+        setIsLoading(false); // Set loading state to false once data is fetched
+      }
+    }
+    search();
   };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      await getId()
+      await fetchArticlesByKeyword()
+    }
+
+    fetchData();
+  }, [])
 
   const handleSearch = async () => {
     // Only navigate if searchValue is not empty
     if (searchValue.trim() !== '') {
-      await fetchArticlesByKeyword(searchValue);
       router.replace("/search/" + searchValue);
     }
   };
 
   return (
-    <View style={styles.container}>
-      <View style={styles.searchRow}>
-        <TextInput
-          style={styles.searchInput}
-          placeholder="Search..."
-          value={searchValue}
-          onChangeText={text => setSearchValue(text)}
-        />
-        <TouchableOpacity onPress={handleSearch}>
-          <icons.notification fill={COLORS.iconColor} />
-        </TouchableOpacity>
+    <ScrollView style={styles.scrollView}>
+      <View style={styles.container}>
+        <Stack.Screen
+          options={{
+            headerTitle: () => <Header title={"Results for \"" + decodedKeyword + "\""} iconvisible={false} />,
+          }} />
+        <View style={styles.searchRow}>
+          <TextInput
+            style={styles.searchInput}
+            placeholder="Search..."
+            value={searchValue}
+            onChangeText={text => setSearchValue(text)}
+          />
+          <TouchableOpacity onPress={handleSearch}>
+            <icons.notification fill={COLORS.iconColor} />
+          </TouchableOpacity>
+        </View>
+        {isLoading ? (
+          <Text>Loading...</Text>
+        ) : articles.length === 0 ? (
+          <Text>No articles found</Text>
+        ) : (
+          <BigArticleList articles={articles} scrollEnabled={false} />
+        )}
       </View>
-      <BigArticleList articles={articles} scrollEnabled={true} />
-    </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
+  scrollView: {
+    flex: 1,
+    backgroundColor: COLORS.primary
+  },
   container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    margin: 15,
+    backgroundColor: COLORS.primary
   },
   searchRow: {
     flexDirection: 'row',
