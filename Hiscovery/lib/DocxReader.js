4ac021e1dd6aad18285ae024c859de336c8be3a0
BaoHaiyah <113325458+BaoHaiyah@users.noreply.github.com>
2024-06-02 08:59:24 +0700

    skeleton for text to speech using expo-speech

--- Hiscovery/lib/DocxReader.js
+++ Hiscovery/lib/DocxReader.js
@@ -1,19 +1,13 @@
 import React, { useState, useEffect } from "react";
-import { Text, View, Dimensions } from "react-native";
+import { Text, View, Dimensions, Button } from "react-native";
 import { WebView } from "react-native-webview";
 import mammoth from "mammoth";
-import { supabase } from "./supabase"; // Import Supabase client
-import { COLORS } from "../constants";
-import { NativeBaseProvider } from "native-base";
+import * as Speech from 'expo-speech'; // Import expo-speech
 
 const DocxReader = ({ docxUrl }) => {
   const [htmlContent, setHtmlContent] = useState(null);
-  const [textContainerWidth, setTextContainerWidth] = useState(
-    Dimensions.get("window").width
-  );
-  const [containerWidth, setContainerWidth] = useState(
-    Dimensions.get("window").width * 0.9
-  );
+  const [textContainerWidth, setTextContainerWidth] = useState(Dimensions.get("window").width);
+  const [plainText, setPlainText] = useState("");
 
   useEffect(() => {
     const fetchDocxAndConvertToHtml = async () => {
@@ -22,6 +16,11 @@ const DocxReader = ({ docxUrl }) => {
         const docxData = await response.arrayBuffer();
         const result = await mammoth.convertToHtml({ arrayBuffer: docxData });
         setHtmlContent(result.value);
+
+        const plainTextResult = await mammoth.extractRawText({
+          arrayBuffer: docxData,
+        });
+        setPlainText(plainTextResult.value);
       } catch (error) {
         console.error("Error reading or converting DOCX:", error);
       }
@@ -34,58 +33,60 @@ const DocxReader = ({ docxUrl }) => {
     setTextContainerWidth(Dimensions.get("window").width - 15);
   }, []);
 
+  const handleReadAloud = () => {
+    Speech.stop();
+    // Speech.speak('Hello World!, This is the article page!');
+    Speech.speak(plainText);
+  };
+
   const injectedJS = `
+    var style = document.createElement('style');
+    style.innerHTML = 'body { font-size: 20px; text-align: justify; font-family: Roboto}';
+    document.head.appendChild(style);
 
-  // Adjust text size and alignment
-  var style = document.createElement('style');
-  style.innerHTML = 'body { font-size: 20px; text-align: justify; font-family: Roboto}'; // Adjust the font-size as desired
-  document.head.appendChild(style);
+    var textContainerWidth = ${textContainerWidth};
+    var images = document.getElementsByTagName('img');
+    for (var i = 0; i < images.length; i++) {
+      images[i].style.width = textContainerWidth + 'px';
+      images[i].style.height = 'auto';
 
-  // Scale images to match text container width
-var textContainerWidth = ${textContainerWidth};
-var images = document.getElementsByTagName('img');
-for (var i = 0; i < images.length; i++) {
-  images[i].style.width = textContainerWidth + 'px';
-  images[i].style.height = 'auto';
+      var imageContainer = document.createElement('div');
+      imageContainer.style.paddingRight = '20px';
+      imageContainer.style.boxSizing = 'border-box';
+      images[i].parentNode.insertBefore(imageContainer, images[i].nextSibling);
+      imageContainer.appendChild(images[i]);
+    }
 
-  // Create a new container for each image and set padding
-  var imageContainer = document.createElement('div');
-  imageContainer.style.paddingRight = '20px'; // Add padding to the right of each image container
-  imageContainer.style.boxSizing = 'border-box'; // Ensure padding doesn't increase container width
-  images[i].parentNode.insertBefore(imageContainer, images[i].nextSibling); // Insert image container after the image
-  imageContainer.appendChild(images[i]); // Append image to the container
-}
+    var links = document.getElementsByTagName('a');
+    for (var j = 0; j < links.length; j++) {
+      var linkContainer = document.createElement('div');
+      linkContainer.style.width = '100%';
+      linkContainer.style.overflow = 'hidden';
+      linkContainer.style.textOverflow = 'ellipsis';
+      linkContainer.style.whiteSpace = 'nowrap';
 
-  // Wrap URL links in a container with specific styles
-  var links = document.getElementsByTagName('a');
-  for (var j = 0; j < links.length; j++) {
-    var linkContainer = document.createElement('div');
-    linkContainer.style.width = '100%'; // Set the container width to match the parent width
-    linkContainer.style.overflow = 'hidden';
-    linkContainer.style.textOverflow = 'ellipsis';
-    linkContainer.style.whiteSpace = 'nowrap';
-
-    links[j].parentNode.insertBefore(linkContainer, links[j]);
-    linkContainer.appendChild(links[j]);
-  }
+      links[j].parentNode.insertBefore(linkContainer, links[j]);
+      linkContainer.appendChild(links[j]);
+    }
   `;
 
   return (
     <View style={{ flex: 1 }}>
       {htmlContent ? (
-        <WebView
-          originWhitelist={["*"]}
-          source={{ html: htmlContent }}
-          style={{ flex: 1 }}
-          injectedJavaScript={injectedJS}
-          javaScriptEnabled={true}
-          scalesPageToFit={false}
-          scrollEnabled={false}
-        />
+        <>
+          <WebView
+            originWhitelist={["*"]}
+            source={{ html: htmlContent }}
+            style={{ flex: 1 }}
+            injectedJavaScript={injectedJS}
+            javaScriptEnabled={true}
+            scalesPageToFit={false}
+            scrollEnabled={false}
+          />
+          <Button title="Read" onPress={handleReadAloud} />
+        </>
       ) : (
-        <View
-          style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
-        >
+        <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
           <Text>Loading...</Text>
         </View>
       )}
